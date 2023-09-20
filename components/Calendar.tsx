0e4328900ba20f7d78d249ac9b31e13238941302
Aloys <17796338+abitbetterthanyesterday@users.noreply.github.com>
2023-09-20 21:44:05 +1000

    Fix the date for GMT < 0

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -14,6 +14,7 @@ export const Calendar = ({
   setSelectedDate,
   steps,
 }: CalendarProps) => {
+  console.log(selectedDate);
   return (
     <div className="shadow-md border-y-2 border-t-slate-100 border-b-slate-300 shadow-lg shadow rounded-lg overflow-hidden bg-white">
       <div className="grid grid-cols-7 grid-rows-1 gap-2 px-8 py-4">
@@ -32,7 +33,10 @@ export const Calendar = ({
         <div />
         <div />
         {new Array(30).fill(null).map((_, i) => {
-          const isSelected = selectedDate.getDate() === i + 1;
+          const isSelected =
+            selectedDate.getTimezoneOffset() > 0
+              ? selectedDate.getDate() === i
+              : selectedDate.getDate() === i + 1;
           const hasSteps = steps.some(
             ({ date, steps }) =>
               Number(date.split("-")[2]) == i + 1 && steps !== 0
@@ -43,9 +47,12 @@ export const Calendar = ({
               className={`flex items-center justify-center hover:ring-2 hover:ring-offset-2 hover:ring-indigo-500 rounded-full p-0 m-0 aspect-square relative p-4 hover:text-slate-900 hover:font-bold text-indigo-900 transition
 cursor-pointer
  ${isSelected ? "font-bold text-indigo-900" : ""}`}
-              onClick={() =>
-                setSelectedDate(new Date(Date.UTC(2023, 8, i + 1, 0, 0, 0)))
-              }
+              onClick={() => {
+                const selectedDate = new Date(
+                  Date.UTC(2023, 8, i + 1, 0, 0, 0)
+                );
+                setSelectedDate(selectedDate);
+              }}
             >
               {hasSteps && (
                 <div className="h-2 w-2 absolute top-3 left-[calc(50% - .125rem)] border-b border-indigo-800 rounded-full bg-indigo-500 z-50"></div>
